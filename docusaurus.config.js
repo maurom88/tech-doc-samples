9eab36025a8023da603734ce8d7939520af8cffb
Mauro Meden <mauro.meden@gmail.com>
2023-05-16 15:33:32 -0600

    Update url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://mauro-tech-doc-samples.github.io',
+  url: 'https://maurom88.github.io/tech-doc-samples/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
