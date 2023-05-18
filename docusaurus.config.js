486595133cb678780c896226a93076b74a28506b
Mauro Meden <mauro.meden@gmail.com>
2023-05-18 08:38:00 -0600

    Fix links in navbar

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -76,8 +76,9 @@ const config = {
             position: 'left',
             label: 'Samples',
           },
+          {to: '/blog', label: 'Blog', position: 'left'},
           {
-            href: 'https://github.com/facebook/docusaurus',
+            href: 'https://github.com/maurom88/tech-doc-samples',
             label: 'GitHub',
             position: 'right',
           },
